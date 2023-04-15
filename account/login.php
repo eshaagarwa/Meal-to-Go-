35c7348cbaa84ac9a738af6d08c96b2f08763410
esha995 <eshaaaagarwal996@gmail.com>
2023-04-15 14:45:01 +0530

    commit 1

--- account/login.php
+++ account/login.php
@@ -14,7 +14,7 @@ else{
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="msapplication-tap-highlight" content="no">
-  <title>Login</title>
+  <title>Login page</title>
 
   <!-- Favicons-->
   <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
