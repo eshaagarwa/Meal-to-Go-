a4273da30e8b8b7dc6a771a21af28f97d60c71e3
esha995 <eshaaaagarwal996@gmail.com>
2023-04-22 04:20:13 +0530

    commit 13

--- food_ordering1/admin/index.php
+++
@@ -1,3 +0,0 @@
-<?php include('top.php');?>
-
-<?php include('footer.php');?>
--- food_ordering1/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-include('function.inc.php');
-unset($_SESSION['IS_LOGIN']);
-redirect('login.php');
-?>
